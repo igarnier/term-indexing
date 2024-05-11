ca8335e841985f1f988f6c284f45791970a27aee
Ilias Garnier <igarnier@protonmail.com>
2024-05-11 09:30:14 +0200

    signature: factor out Hashed module type

--- lib/signature.ml
+++ lib/signature.ml
@@ -1,9 +1,8 @@
-(** The type of signatures of first-order terms. *)
-module type S = sig
-  (** The type of symbols. *)
+(** Hashable, totally ordered, printable types *)
+module type Hashed = sig
   type t
 
-  (** [compare] is a total order on symbols. *)
+  (** [compare] is a total order. *)
   val compare : t -> t -> int
 
   (** [equal s1 s2] tests whether [s1] and [s2] are equal. *)
@@ -14,6 +13,11 @@ module type S = sig
 
   (** [pp fmt s] prints a representation of [s] to the formatter [fmt]. *)
   val pp : Format.formatter -> t -> unit
+end
+
+(** The type of signatures of first-order terms. *)
+module type S = sig
+  include Hashed
 
   (** [arity s] is the arity of [s], i.e. a term with head [s] must have exactly [arity s] sub-terms. *)
   val arity : t -> int
