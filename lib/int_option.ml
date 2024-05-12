d86a5a3febd148b19d6e119ec18facfd9c3c6dc1
Ilias Garnier <igarnier@protonmail.com>
2024-05-12 18:27:06 +0200

    int_option: smarter representational choice

--- lib/int_option.ml
+++ lib/int_option.ml
@@ -1,10 +1,12 @@
 type t = int
 
-let none = Int.max_int
+let none = Int.min_int
 
 let of_int x =
-  if x = Int.max_int - 1 then invalid_arg "Int_option.of_int" else x + 1
+  if x = Int.max_int then invalid_arg "Int_option.of_int" else x + 1
 
 let elim x ifnone ifsome = if x = none then ifnone else ifsome (x - 1)
 
 let is_none x = x = none
+
+let max = Int.max
--- lib/int_option.mli
+++ lib/int_option.mli
@@ -1,4 +1,5 @@
-(** A space-efficient encoding of [int option] *)
+(** A space-efficient encoding of [int option]. Only caveat: one cannot represent
+    [Some Int.max_int]. *)
 type t
 
 val none : t
@@ -8,3 +9,5 @@ val of_int : int -> t
 val elim : t -> 'a -> (int -> 'a) -> 'a
 
 val is_none : t -> bool
+
+val max : t -> t -> t
